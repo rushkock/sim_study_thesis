a5ada81f6be13b02be51866d4d6a5180e97b8d3d
Ruchella Kock <ruchella_kock@hotmail.com>
2019-04-13 10:56:27 +0200

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simulation study comparing the performance of the Welch t-test and the permutation test under the violation of homogeneity',
+    author='R. Kock',
+    license='MIT',
+)
